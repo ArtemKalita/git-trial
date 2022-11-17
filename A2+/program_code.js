5d3fc054ddc1ca03f07f573a62a8ab31596d236f
artem kalita <a1@MacBook-Pro-Al.local>
2022-11-17 22:11:53 +0300

    A2+

--- A2+/program_code.js
+++ A2+/program_code.js
@@ -1,16 +1,12 @@
 const str1 = prompt("введите текст");
 const function_delit_spase = (str_change) => {
-  let i = 0;
-  let a = 0;
-  while (str_change[i] == " ") {
+  while (str_change[0] == " ") {
     str_change = str_change.substring(1);
-    i += 1;
   }
-  while (str_change[str_change.length - a] == " ") {
-    console.log(str_change[str_change.length - a] == " ");
-    str_change = str_change.substring(-1);
-    a += 1;
+  while (str_change[str_change.length - 1] == " ") {
+    str_change = str_change.substring(0, str_change.length - 1);
   }
+
   return str_change;
 };
 alert(function_delit_spase(str1));
