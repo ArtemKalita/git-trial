037f21115b406810953c0d4b97b4d1e9897792b9
artem kalita <a1@192.168.100.8>
2022-12-24 21:12:22 +0300

    DRINKS_HASH_FUNC

--- DRINKS_HASH_FUNC/DRINKS_HASH_FUNC.js
+++ DRINKS_HASH_FUNC/DRINKS_HASH_FUNC.js
@@ -1,33 +1,44 @@
-class DRINKS_HASH_FUNC {
+class HashStorageClass {
   constructor() {
     this.obj = {};
   }
-  addValue = (key, value) => {
-    this.obj[key] = value;
-    return this.obj;
+  addValue = (key, alcoholic, ...value) => {
+    if (alcoholic) {
+      alcoholic = "Да";
+    } else {
+      alcoholic = "Нет";
+    }
+    this.obj[key] = [alcoholic, value];
+
+    alert("Напиток добавлен");
   };
   getValu = (key) => {
-    return this.obj[key];
+    alert(`Напиток:${key}
+Алкагольный: ${this.obj[key][0]}
+Ингридиенты:${this.obj[key][1]}`);
   };
   deleteValue = (key) => {
     if (!(key in this.obj)) {
-      return false;
+      alert("Напиток не найден");
     } else {
       delete this.obj[key];
-      return true;
+      alert("Напиток удален");
     }
   };
   getKeys = () => {
-    return Object.keys(this.obj);
+    if (Object.keys(this.obj).length === 0) {
+      alert("Напитков нет");
+    } else {
+      alert(`Названия всех напитков: ${Object.keys(this.obj)}`);
+    }
   };
 }
 
-const name1 = new DRINKS_HASH_FUNC("rgrg", "grgrt");
+// const name1 = new DRINKS_HASH_FUNC(); //1 задание
 
-console.log(
-  name1.addValue("rgrg", "1"),
-  name1.addValue("rgrg1", "2"),
-  name1.getValu("rgrg"),
-  name1.deleteValue("rgrg"),
-  name1.getKeys()
-);
+// console.log(name1.addValue("rgrg", "1", 4, "f"));
+// console.log(name1.addValue("rgrg1", "1", 4, "f", "gvtfv"));
+// console.log(name1.getValu("rgrg"));
+// console.log(name1.deleteValue("6"));
+// console.log(name1.getKeys());
+const drinkStorage = new DRINKS_HASH_FUNC();
